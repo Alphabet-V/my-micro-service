46fb94d3ec404376396203e3fd1fe583d3da4f25
liangchanghong <lchong1989@163.com>
2018-11-11 22:26:43 +0800

    learning

--- person/src/main/java/com/lch/it/person/PersonApplication.java
+++ person/src/main/java/com/lch/it/person/PersonApplication.java
@@ -2,10 +2,12 @@ package com.lch.it.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient // 服务注册中心客户端
+@EnableEurekaClient // 服务注册中心客户端  这个只是Eureka用
+@EnableDiscoveryClient // 这个比较通用
 public class PersonApplication {
 
 	public static void main(String[] args) {
